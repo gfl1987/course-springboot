b3518d072487f1459c27c4a34ce11e010a683d96
Guilherme Fonseca Leite <guilherme.leite1987@gmail.com>
2022-03-14 18:50:46 -0300

    Project Created

---
+++ src/main/java/com/gflinformatica/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.gflinformatica.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication { //classe principal do programa
+
+	public static void main(String[] args) { //método main do Java
+		SpringApplication.run(CourseApplication.class, args); //primeiro comando SpringBoot para aplicação rodar.
+	}
+
+}
