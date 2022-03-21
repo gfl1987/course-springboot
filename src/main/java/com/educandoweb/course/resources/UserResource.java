63bd73d9df06ae5b3599352768909e8859951188
Guilherme Fonseca Leite <guilherme.leite1987@gmail.com>
2022-03-21 15:32:58 -0300

    H2 database, teste profile, JPA

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,11 +1,11 @@
-package com.gflinformatica.course.resources;
+package com.educandoweb.course.resources;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gflinformatica.course.entities.User;
+import com.educandoweb.course.entities.User;
 
 @RestController 
 @RequestMapping(value = "/users")
